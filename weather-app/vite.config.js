43980c9b844fbb32194177e553e14f63e8c05b44
malak-y <145293320+malak-y@users.noreply.github.com>
2025-02-28 16:08:02 +0200

    update

--- weather-app/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: "/weather-app/", // Use your GitHub repo name here
-  plugins: [react()],
-});
